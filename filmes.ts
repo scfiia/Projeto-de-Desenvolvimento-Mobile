eeb30c77191abe3f5ac4e421c302844e5508a696
scfiia <rodriguesofia02@gmail.com>
2025-10-02 18:14:32 -0300

    Create filmes.ts

---
+++ filmes.ts
@@ -0,0 +1,32 @@
+[
+  {
+    "id": "1",
+    "titulo": "Matrix",
+    "ano": 1999,
+    "genero": "Ficção Científica",
+  },
+  {
+    "id": "2",
+    "titulo": "Interestelar",
+    "ano": 2014,
+    "genero": "Ficção Científica",
+  },
+  {
+    "id": "3",
+    "titulo": "Parasita",
+    "ano": 2019,
+    "genero": "Drama",
+  },
+  {
+    "id": "4",
+    "titulo": "Coringa",
+    "ano": 2019,
+    "genero": "Drama",
+  },
+  {
+    "id": "5",
+    "titulo": "Vingadores Ultimato",
+    "ano": 2019,
+    "genero": "Ação",
+  }
+]
